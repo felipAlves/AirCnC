579fc23c84c4d72cf80dda9988b44e3b633ed006
Felipe Garcia Alves <47622464+felipAlves@users.noreply.github.com>
2020-08-30 23:16:12 -0300

    Criando backend da aplicação

---
+++ api/src/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const routes = require('./routes')
+
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://felipealves:felipealves@cluster0.wsu5r.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const app = express()
+
+
+// GET, PUT, POST, DELETE
+
+// req.query = Acessar query params (para Filtros)
+// req.params = Acessar route params (para edição, delete)
+
+app.use(express.json())
+app.use(routes)
+
+
+app.listen(3333)
